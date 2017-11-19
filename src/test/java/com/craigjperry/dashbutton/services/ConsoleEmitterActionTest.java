0b915f1a6bd44dabb78aca1a656bd4ba69aff933
Craig J Perry <craigp84@gmail.com>
2017-11-19 20:57:11 +0000

    refactor(console): Take advantage of the new ConsoleWriter

--- src/test/java/com/craigjperry/dashbutton/services/ConsoleEmitterActionTest.java
+++ src/test/java/com/craigjperry/dashbutton/services/ConsoleEmitterActionTest.java
@@ -1,49 +1,48 @@
 package com.craigjperry.dashbutton.services;
 
 import com.craigjperry.dashbutton.DashButton;
+import com.craigjperry.dashbutton.drivers.ConsoleWriter;
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.runners.MockitoJUnitRunner;
 
 import java.util.function.Consumer;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
-
+@RunWith(MockitoJUnitRunner.class)
 public class ConsoleEmitterActionTest {
+
+    @Mock
+    ConsoleWriter mockConsoleWriter;
 
     @Test
     public void performEmitsTheConfiguredMessage() throws Exception {
         // Given
-        ConsoleSpy mockConsole = new ConsoleSpy();
-        ConsoleEmitterAction action = new ConsoleEmitterAction(mockConsole, "Hello");
+        ConsoleEmitterAction action = new ConsoleEmitterAction(mockConsoleWriter, "Hello");
 
         // When
         action.perform(new DashButton("00:12:34:56:78:00"));
 
         // Then
-        assertThat(mockConsole.getMessage()).isEqualTo("Hello");
+        verify(mockConsoleWriter).println("Hello");
     }
 
     @Test
     public void noCallToPerformEmitsNothing() throws Exception {
         // Given
-        ConsoleSpy mockConsole = new ConsoleSpy();
-        ConsoleEmitterAction action = new ConsoleEmitterAction(mockConsole, "Hello");
+        ConsoleEmitterAction action = new ConsoleEmitterAction(mockConsoleWriter, "Hello");
 
         // When perform() is NOT invoked ...
 
         // Then
-        assertThat(mockConsole.getMessage()).isNull();
-    }
-
-    private static class ConsoleSpy implements Consumer<String> {
-        private String message;
-
-        public void accept(String message) {
-            this.message = message;
-        }
-
-        public String getMessage() {
-            return message;
-        }
+        verify(mockConsoleWriter, never()).println(anyString());
     }
 }
