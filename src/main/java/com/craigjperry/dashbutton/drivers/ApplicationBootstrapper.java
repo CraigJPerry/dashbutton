bcf7a809f8a54cb5fd6e0631b517afa9252188dd
Craig J Perry <craigp84@gmail.com>
2017-11-19 22:27:02 +0000

    config(console): Add console emitter bean to application context

--- src/main/java/com/craigjperry/dashbutton/drivers/ApplicationBootstrapper.java
+++ src/main/java/com/craigjperry/dashbutton/drivers/ApplicationBootstrapper.java
@@ -1,6 +1,7 @@
 package com.craigjperry.dashbutton.drivers;
 
 import com.craigjperry.dashbutton.DashButton;
+import com.craigjperry.dashbutton.services.ConsoleEmitterAction;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -8,22 +9,27 @@ import org.springframework.context.annotation.Bean;
 @SpringBootApplication(scanBasePackages = "com.craigjperry.dashbutton")
 public class ApplicationBootstrapper {
 
-	@Bean
-	public DashButton dashButton1() {
-		return new DashButton("00:12:34:56:78:00");
-	}
+    @Bean
+    public DashButton dashButton1() {
+        return new DashButton("00:12:34:56:78:00");
+    }
 
-	@Bean
-	public DashButton dashButton2() {
-		return new DashButton("00:00:12:34:56:78");
-	}
+    @Bean
+    public DashButton dashButton2() {
+        return new DashButton("00:00:12:34:56:78");
+    }
 
-	@Bean
-	public ConsoleWriter consoleWriter() {
-		return new ConsoleWriter(System.out::println);
-	}
+    @Bean
+    public ConsoleWriter consoleWriter() {
+        return new ConsoleWriter(System.out::println);
+    }
 
-	public static void main(String[] args) {
-		SpringApplication.run(ApplicationBootstrapper.class, args);
-	}
+    @Bean
+    public ConsoleEmitterAction consoleEmitterAction() {
+        return new ConsoleEmitterAction(consoleWriter(), "Button was pressed!");
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationBootstrapper.class, args);
+    }
 }
