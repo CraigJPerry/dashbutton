48578cf6dc3d969287a82c0fdb1ce4a743709b7a
Craig J Perry <craigp84@gmail.com>
2017-11-19 19:15:38 +0000

    chore(setup): Initial import from spring initializr

---
+++ src/main/java/com/craigjperry/dashbutton/DashButtonApplication.java
@@ -0,0 +1,11 @@
+package com.craigjperry.dashbutton;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DashButtonApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(DashButtonApplication.class, args);
+	}
+}
