82843f1d1eb13f54d73498976634d162cf7408ba
Craig J Perry <craigp84@gmail.com>
2017-11-21 21:29:39 +0000

    feat(dispatch): Dispatch known button presses to configured actions

---
+++ src/main/java/com/craigjperry/dashbutton/drivers/NetworkInterfaceChooser.java
@@ -0,0 +1,20 @@
+package com.craigjperry.dashbutton.drivers;
+
+import org.pcap4j.core.PcapNativeException;
+import org.pcap4j.core.PcapNetworkInterface;
+import org.pcap4j.core.Pcaps;
+import org.springframework.stereotype.Component;
+
+import java.net.Inet4Address;
+import java.net.UnknownHostException;
+
+@Component
+public class NetworkInterfaceChooser {
+    public PcapNetworkInterface firstLocalInterface() {
+        try {
+            return Pcaps.getDevByAddress(Inet4Address.getLocalHost());
+        } catch (PcapNativeException|UnknownHostException e) {
+            throw new IllegalStateException("pcap library initialisation failed because: " + e);
+        }
+    }
+}
