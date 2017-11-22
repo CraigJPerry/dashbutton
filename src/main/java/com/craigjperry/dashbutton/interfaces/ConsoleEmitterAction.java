7d295d511ee92903539b8bee19dac8728b37be3a
Craig J Perry <craigp84@gmail.com>
2017-11-22 02:01:53 +0000

    chore(autowiring): Setup autowiring

--- src/main/java/com/craigjperry/dashbutton/interfaces/ConsoleEmitterAction.java
+++ src/main/java/com/craigjperry/dashbutton/interfaces/ConsoleEmitterAction.java
@@ -3,12 +3,15 @@ package com.craigjperry.dashbutton.interfaces;
 import com.craigjperry.dashbutton.Action;
 import com.craigjperry.dashbutton.DashButton;
 import com.craigjperry.dashbutton.drivers.ConsoleWriter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
+@Component
 public class ConsoleEmitterAction implements Action {
     private final ConsoleWriter consoleWriter;
     private final String message;
 
-    public ConsoleEmitterAction(ConsoleWriter consoleWriter, String message) {
+    public ConsoleEmitterAction(ConsoleWriter consoleWriter, @Value("${console.emitter.action.message}") String message) {
         this.consoleWriter = consoleWriter;
         this.message = message;
     }
